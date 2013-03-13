f3cf6c3b44ba747708fe67d166a53d06af9cf248
hecq <hecq@novasoftware.cn>
2013-03-13 14:51:42 +0800

    Add Common Controller
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Leads/Controllers/LeadController.cs
+++ src/Orchard.Web/Modules/Coevery.Leads/Controllers/LeadController.cs
@@ -10,37 +10,36 @@ using Orchard.WebApi.Common;
 
 namespace Coevery.Leads.Controllers
 {
-    public class LeadController : RecordController<LeadRecord,LeadDto> {
-        
-
+    public class LeadController : RecordController<LeadRecord,LeadDto>
+    {
         public LeadController(IRepository<LeadRecord> leadRepository)
             :base(leadRepository)
         {
             
         }
-
-        // GET api/leads/lead
-        public IEnumerable<LeadDto> GetLeads() {
+       
+        public IEnumerable<LeadDto> GetLeads() 
+        {
             return GetRecords();
         }
-
-        // GET api/leads/lead/5
-        public LeadDto GetLead(int id) {
+        
+        public LeadDto GetLead(int id)
+        {
             return GetRecord(id);
         }
-
-        // PUT api/leads/lead/5
-        public HttpResponseMessage PutLead(int id, LeadDto leadDto) {
+        
+        public HttpResponseMessage PutLead(int id, LeadDto leadDto) 
+        {
             return PutRecord(id, leadDto);
         }
-
-        // POST api/leads/lead
-        public HttpResponseMessage PostLead(LeadDto leadDto) {
+       
+        public HttpResponseMessage PostLead(LeadDto leadDto)
+        {
             return PostRecord(leadDto);
         }
-
-        // DELETE api/leads/lead/5
-        public HttpResponseMessage DeleteLead(int id) {
+      
+        public HttpResponseMessage DeleteLead(int id) 
+        {
             return DeleteRecord(id);
         }
     }
