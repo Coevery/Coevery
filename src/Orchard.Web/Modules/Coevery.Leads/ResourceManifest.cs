f595a339eb27e6a6b1c62eb65dc48bd1a6edcbc7
hecq <hecq@novasoftware.cn>
2013-03-15 09:48:56 +0800

    devide angularjs
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Leads/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Coevery.Leads/ResourceManifest.cs
@@ -7,19 +7,14 @@ namespace Coevery.Leads
         public void BuildManifests(ResourceManifestBuilder builder)
         {
             var manifest = builder.Add();
-            manifest.DefineStyle("ngGrid").SetUrl("ng-grid.css");
-            manifest.DefineStyle("LeadMain").SetUrl("todo.css");
-            manifest.DefineStyle("pnotify").SetUrl("jquery.pnotify.css");
-
-            manifest.DefineScript("angular").SetUrl("angular.js");
-            manifest.DefineScript("angularResource").SetUrl("angular-resource.js");
-            manifest.DefineScript("ngGrid").SetUrl("ng-grid-2.0.0.debug.js");
+        
+           
             manifest.DefineScript("LeadMain").SetUrl("app/main.js");
             manifest.DefineScript("LeadLogger").SetUrl("app/logger.js");
             manifest.DefineScript("LeadContext").SetUrl("app/context.js");
             manifest.DefineScript("LeadController").SetUrl("app/controller.js");
             manifest.DefineScript("LeadDetailController").SetUrl("app/detailcontroller.js");
-            manifest.DefineScript("pnotify").SetUrl("jquery.pnotify.min.js");
+          
         }
     }
 }
