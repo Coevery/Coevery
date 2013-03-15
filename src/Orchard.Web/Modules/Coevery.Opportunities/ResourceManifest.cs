241d7a79a8bb21400cb5a695b7efcecf6d7c5a84
hecq <hecq@novasoftware.cn>
2013-03-15 09:49:08 +0800

    devide angularjs
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Opportunities/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Coevery.Opportunities/ResourceManifest.cs
@@ -1,25 +1,17 @@
 ﻿using Orchard.UI.Resources;
 
-namespace Coevery.Leads
+namespace Coevery.Opportunities
 {
     public class ResourceManifest : IResourceManifestProvider
     {
         public void BuildManifests(ResourceManifestBuilder builder)
         {
             var manifest = builder.Add();
-            manifest.DefineStyle("ngGrid").SetUrl("ng-grid.css");
-            manifest.DefineStyle("OpportunityMain").SetUrl("todo.css");
-            manifest.DefineStyle("pnotify").SetUrl("jquery.pnotify.css");
-
-            manifest.DefineScript("angular").SetUrl("angular.js");
-            manifest.DefineScript("angularResource").SetUrl("angular-resource.js");
-            manifest.DefineScript("ngGrid").SetUrl("ng-grid-2.0.0.debug.js");
             manifest.DefineScript("OpportunityMain").SetUrl("app/main.js");
             manifest.DefineScript("OpportunityLogger").SetUrl("app/logger.js");
             manifest.DefineScript("OpportunityContext").SetUrl("app/context.js");
             manifest.DefineScript("OpportunityController").SetUrl("app/controller.js");
             manifest.DefineScript("OpportunityDetailController").SetUrl("app/detailcontroller.js");
-            manifest.DefineScript("pnotify").SetUrl("jquery.pnotify.min.js");
         }
     }
 }
