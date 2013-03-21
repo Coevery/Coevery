ca0bff15c48758c1420382dece7174c11d2332ec
sword <none@none>
2013-03-21 10:31:05 +0800

    Update AngularJS module
    --HG--
    branch : 1.x
    rename : src/Orchard.Web/Modules/Orchard.AngularJs/Orchard.AngularJs.csproj => src/Orchard.Web/Modules/Orchard.AngularJs/Orchard.AngularJS.csproj

--- src/Orchard.Web/Modules/Orchard.AngularJs/ResourceManifest.cs
+++ src/Orchard.Web/Modules/Orchard.AngularJs/ResourceManifest.cs
@@ -1,18 +1,24 @@
 using Orchard.UI.Resources;
 
-namespace Orchard.jQuery {
-    public class ResourceManifest : IResourceManifestProvider {
-        public void BuildManifests(ResourceManifestBuilder builder) {
+namespace Orchard.AngularJS
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
             var manifest = builder.Add();
 
-            manifest.DefineStyle("TodoStyle").SetUrl("todo.css");
+            manifest.DefineScript("angular").SetUrl("angular.min.js", "angular.js").SetVersion("1.0.5")
+                .SetCdn("//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js", "//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.js", true);
+
+            manifest.DefineScript("angularResource").SetUrl("angular-resource.min.js", "angular-resource.js").SetVersion("1.0.5")
+                .SetCdn("//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js", "//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.js", true);
+
+            manifest.DefineScript("ngGrid").SetUrl("ng-grid-2.0.2.min.js", "ng-grid-2.0.2.debug.js").SetVersion("2.0.2");
             manifest.DefineStyle("ngGrid").SetUrl("ng-grid.css");
+
             manifest.DefineStyle("pnotify").SetUrl("jquery.pnotify.css");
-            manifest.DefineScript("angular").SetUrl("angular.js");
-            manifest.DefineScript("angularResource").SetUrl("angular-resource.js");
-            manifest.DefineScript("ngGrid").SetUrl("ng-grid-2.0.0.debug.js");
             manifest.DefineScript("pnotify").SetUrl("jquery.pnotify.min.js");
-          
         }
     }
 }
