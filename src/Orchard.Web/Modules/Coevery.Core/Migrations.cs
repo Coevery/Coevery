0d5b9ef94b159d04b98d75e8fed06e6e070df0c3
hecq <hecq@novasoftware.cn>
2013-03-22 11:00:19 +0800

    Add Coevery.Navigation
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.Core/Migrations.cs
@@ -0,0 +1,28 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+
+namespace Coevery.Core {
+    public class Migrations : DataMigrationImpl {
+        public int Create() {
+            SchemaBuilder.CreateTable("ModuleMenuItemPartRecord",
+                table => table
+                    .ContentPartRecord()
+                    .Column<int>("ModuleMenuItemRecord_id")
+                );
+
+            ContentDefinitionManager.AlterTypeDefinition("ModuleMenuItem", cfg => cfg
+                .WithPart("MenuPart")
+                .WithPart("CommonPart")
+                .WithPart("IdentityPart")
+                .WithPart("ModuleMenuItemPart")
+                .DisplayedAs("Module Menu Item")
+                .WithSetting("Description", "Adds a Module Menu Item to navigation")
+                .WithSetting("Stereotype", "MenuItem")
+                );
+
+            return 1;
+        }
+
+    
+    }
+}
---
+++ src/Orchard.Web/Modules/Coevery.Core/ViewModels/ModuleMenuItemEditViewModel.cs
@@ -0,0 +1,8 @@
+using Coevery.Core.Models;
+
+namespace Coevery.Core.ViewModels {
+    public class ModuleMenuItemEditViewModel  {
+        public int ContentItemId { get; set; }
+        public ModuleMenuItemPart Part { get; set; }
+    }
+}
