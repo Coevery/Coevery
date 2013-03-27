0ed3154cceb5f2bbf78cbc381ab4d961a9f05fb9
hecq <hecq@novasoftware.cn>
2013-03-27 17:50:29 +0800

    Add metadata link
    --HG--
    branch : 1.x

---
+++ src/Orchard/ContentManagement/MetaData/ContentDefinitionManagerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Orchard.ContentManagement.MetaData.Models;
+
+namespace Orchard.ContentManagement.MetaData
+{
+    public static class ContentDefinitionServiceExtension
+    {
+        public static IEnumerable<ContentTypeDefinition> ListUserTypeDefinitions(this IContentDefinitionManager contentDefinitionManager)
+        {
+            var contentyTypes = contentDefinitionManager.ListTypeDefinitions();
+            var typeNames = contentyTypes.Select(ctd => ctd.Name);
+            var parts = contentDefinitionManager.ListPartDefinitions();
+            var userParts = parts.Where(cpd => typeNames.Contains(cpd.Name));
+            return contentyTypes.Where(c => userParts.Any(u => u.Name == c.Name));
+        }
+    }
+}
