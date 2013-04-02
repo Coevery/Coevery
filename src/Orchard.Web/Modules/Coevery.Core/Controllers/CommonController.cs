b09ef9d81fe2591086355daa259c0c284eca8f34
hecq <hecq@novasoftware.cn>
2013-04-02 18:22:37 +0800

    ng-Grid layout
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Core/Controllers/CommonController.cs
+++ src/Orchard.Web/Modules/Coevery.Core/Controllers/CommonController.cs
@@ -9,7 +9,7 @@ namespace Coevery.Core.Controllers
     public class CommonController :ApiController
     {
         // GET api/leads/lead
-        public virtual IEnumerable<object> Get()
+        public  IEnumerable<object> Get()
         {
             List<object> re = new List<object>();
            re.Add( new {Topic = "Topic",FirstName = "my",LastName = "you",StatusCode = "1"});
