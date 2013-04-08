34c44a8330c380dd2c571285138c141d201841f8
hecq <hecq@novasoftware.cn>
2013-04-08 16:45:19 +0800

    FormDesigner-FieldDrag
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Metadata/Controllers/HomeController.cs
+++ src/Orchard.Web/Modules/Coevery.Metadata/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using Coevery.Metadata.HtmlControlProviders;
+using Coevery.Metadata.Services;
 using Coevery.Metadata.ViewModels;
 using Orchard;
 using Orchard.ContentManagement.MetaData;
@@ -15,13 +17,13 @@ namespace Coevery.Metadata.Controllers {
     public class HomeController : Controller
     {
         private IContentDefinitionManager _contentDefinitionManager;
-       
-        public HomeController(
-            IOrchardServices orchardServices,
-            IContentDefinitionManager contentDefinitionManager
-            )
+        private IContentDefinitionService _contentDefinitionService;
+        public HomeController(IOrchardServices orchardServices,
+            IContentDefinitionManager contentDefinitionManager,
+            IContentDefinitionService contentDefinitionService)
         {
             _contentDefinitionManager = contentDefinitionManager;
+            _contentDefinitionService = contentDefinitionService;
             Services = orchardServices;
             T = NullLocalizer.Instance;
         }
@@ -42,7 +44,7 @@ namespace Coevery.Metadata.Controllers {
             var typeDefinition = _contentDefinitionManager.GetTypeDefinition(id);
             viewModel.TypeDefinition = typeDefinition;
             viewModel.ContentPart = typeDefinition.Parts.FirstOrDefault(t => t.PartDefinition.Name == typeDefinition.Name);
-
+            viewModel.HtmlControlDescs = _contentDefinitionService.GetHtmlDescForTypeFields(viewModel.ContentPart);
             return View(viewModel);
         }
 
