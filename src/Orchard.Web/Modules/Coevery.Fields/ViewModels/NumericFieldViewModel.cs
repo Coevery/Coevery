418901213a831a0237994ccc2b8530e11942bf94
Zhang Junnan <zhangjn0430@gmail.com>
2013-04-10 16:57:17 +0800

    add Coevery.Fields modify form designer
    --HG--
    branch : 1.x
    rename : src/Orchard.Web/Modules/Coevery.Metadata/Controllers/HomeController.cs => src/Orchard.Web/Modules/Coevery.Metadata/Controllers/FormDesignerViewTemplateController.cs
    rename : src/Orchard.Web/Modules/Coevery.Metadata/Views/Home/Dynamic.cshtml => src/Orchard.Web/Modules/Coevery.Metadata/Views/FormDesignerViewTemplate/Dynamic.cshtml

---
+++ src/Orchard.Web/Modules/Coevery.Fields/ViewModels/NumericFieldViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Coevery.Fields.Fields;
+using Coevery.Fields.Settings;
+namespace Coevery.Fields.ViewModels {
+
+    public class NumericFieldViewModel {
+        public NumericField Field { get; set; }
+        public NumericFieldSettings Settings { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Coevery.Metadata/Controllers/FormDesignerViewTemplateController.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Coevery.Metadata.HtmlControlProviders;
+using Coevery.Metadata.Services;
+using Coevery.Metadata.ViewModels;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.Themes;
+using System.Linq;
+
+namespace Coevery.Metadata.Controllers
+{
+
+    public class FormDesignerViewTemplateController : Controller
+    {
+        private IContentDefinitionManager _contentDefinitionManager;
+        private IContentDefinitionService _contentDefinitionService;
+        private readonly IContentManager _contentManager;
+
+        public FormDesignerViewTemplateController(IOrchardServices orchardServices,
+            IContentDefinitionManager contentDefinitionManager,
+            IContentDefinitionService contentDefinitionService,
+            IContentManager contentManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+            _contentDefinitionService = contentDefinitionService;
+            _contentManager = contentManager;
+            Services = orchardServices;
+            T = NullLocalizer.Instance;
+        }
+
+        public IOrchardServices Services { get; private set; }
+        public Localizer T { get; set; }
+        public ILogger Logger { get; set; }
+
+
+        public ActionResult Dynamic(string id, string returnUrl)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            //FormDesignerViewModel viewModel = new FormDesignerViewModel();
+            //var typeDefinition = _contentDefinitionManager.GetTypeDefinition(id);
+            //viewModel.TypeDefinition = typeDefinition;
+            //viewModel.ContentPart = typeDefinition.Parts.FirstOrDefault(t => t.PartDefinition.Name == typeDefinition.Name);
+            //viewModel.HtmlControlDescs = _contentDefinitionService.GetHtmlDescForTypeFields(viewModel.ContentPart);
+
+            var contentItem = _contentManager.New(id);
+            dynamic model = _contentManager.BuildEditor(contentItem);
+            var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(id);
+            string layout = contentTypeDefinition.Settings.ContainsKey("Layout")
+                                ? contentTypeDefinition.Settings["Layout"]
+                                : null;
+            model.Layout = layout;
+
+            return View((object)model);
+        }
+
+    }
+}
