7f34b26a005ae9ae7f164fbc80c0cc60e7d01c71
llb <llb@novasoftware.cn>
2013-04-11 15:27:56 +0800

    commit navigation
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.CoeveryNavigation/Scripts/controllers/navigationListcontroller.js
@@ -0,0 +1,33 @@
+﻿function NavigationListCtrl($scope, logger, $state, $stateParams, $resource) {
+    $scope.saveAll = function () {
+        $.ajax({
+            url: myForm.action,
+            type: myForm.method,
+            data: $(myForm).serialize() + '&submit.Save=Save',
+            success: function (result) {
+                //$state.transitionTo('Detail', { Module: 'Metadata', Id: $stateParams.Id });
+            }
+        });
+    };
+    
+    $scope.edit = function (id) {
+            $state.transitionTo('Detail', { Module: 'CoeveryNavigation', Id: id });
+    };
+    
+    $scope.add = function (id,menuId) {
+        $state.transitionTo('SubDetail', { Module: 'CoeveryNavigation', SubModule: '', View: 'Create', Id: id, SubId: menuId });
+    };
+
+    $scope.delete = function(id,url) {
+        $.ajax({
+            url: url,
+            type: 'post',
+            data: {id: id, __RequestVerificationToken: $("input[name=__RequestVerificationToken]").val()},
+            success: function(result) {
+                //$state.transitionTo('Detail', { Module: 'Metadata', Id: $stateParams.Id });
+            }
+        });
+    };
+}
+
+//@ sourceURL=Coevery.CoeveryNavigation/navigationListcontroller.js
