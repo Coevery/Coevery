705ef33d235683e7a581732b82d9712d4d20c671
llb <llb@novasoftware.cn>
2013-06-19 11:05:34 +0800

    commit projection project
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.Projections/ViewModels/ProjectionEditViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Projections.Descriptors.Property;
+using Orchard.Projections.ViewModels;
+
+namespace Coevery.Projections.ViewModels
+{
+    public class ProjectionEditViewModel
+    {
+        public ProjectionEditViewModel()
+        {
+            QueryViewModel = new AdminEditViewModel();
+            LayoutViewModel = new LayoutEditViewModel();
+            AllFields = new List<PropertyDescriptor>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public AdminEditViewModel QueryViewModel { get; set; }
+        public LayoutEditViewModel LayoutViewModel { get; set; }
+        public IEnumerable<PropertyDescriptor> AllFields { get; set; }
+        public string VisableTo { get; set; }
+        public string SortedBy { get; set; }
+        public string SortMode { get; set; }
+
+    }
+}
