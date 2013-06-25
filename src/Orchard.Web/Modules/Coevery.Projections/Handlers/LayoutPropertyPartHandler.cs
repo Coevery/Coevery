890836a8fe904c8c8518de4b021b41067274accb
llb <llb@novasoftware.cn>
2013-06-25 15:15:17 +0800

    commit front meun and projection bug
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.Projections/Handlers/LayoutPropertyPartHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Coevery.Projections.Models;
+using Orchard.Data;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+
+namespace Coevery.Projections.Handlers {
+    public class LayoutPropertyPartHandler : ContentHandler {
+
+        public LayoutPropertyPartHandler(IRepository<LayoutPropertyRecord> repository)
+        {
+            Filters.Add(StorageFilter.For(repository));
+
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Coevery.Projections/Models/LayoutPropertyRecord.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.ContentManagement.Records;
+using Orchard.Projections.Models;
+
+namespace Coevery.Projections.Models
+{
+    public class LayoutPropertyRecord : ContentPartRecord
+    {
+        public virtual string VisableTo { get; set; }
+        public virtual int PageRowCount { get; set; }
+        public virtual int QueryPartRecord_id { get; set; }
+    }
+}
