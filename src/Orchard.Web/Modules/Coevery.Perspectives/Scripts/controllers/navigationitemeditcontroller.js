09c38baf92b7a67699e78160fbb79f85a150dc0e
llb <llb@novasoftware.cn>
2013-06-27 14:08:24 +0800

    commit swith entity bug
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.Perspectives/Scripts/controllers/navigationitemeditcontroller.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+define(['core/app/detourService'], function (detour) {
+    detour.registerController([
+       'NavigationItemEditCtrl',
+       ['$timeout', '$scope', 'logger', '$detour', '$stateParams', '$resource',
+       function ($timeout, $scope, logger, $detour, $stateParams, $resource) {
+           
+           $scope.exit = function () {
+               $detour.transitionTo('PerspectiveDetail', { Id: $stateParams.Id });
+           };
+
+           $scope.save = function () {
+               $.ajax({
+                   url: myForm.action,
+                   type: myForm.method,
+                   data: $(myForm).serialize() + '&submit.Save=Save',
+                   success: function (result) {
+                       logger.success("Perspective Saved.");
+                   }
+               });
+           };
+       }]
+    ]);
+});
+
+//@ sourceURL=Coevery.Perspectives/navigationitemdetailcontroller.js
