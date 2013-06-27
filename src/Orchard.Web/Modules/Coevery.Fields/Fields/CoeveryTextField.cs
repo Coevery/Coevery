d476345ca62b086b0e0ce39c8d9751359ddd5eaf
llb <llb@novasoftware.cn>
2013-06-27 16:41:19 +0800

    commit fetch by name
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Fields/Fields/CoeveryTextField.cs
+++ src/Orchard.Web/Modules/Coevery.Fields/Fields/CoeveryTextField.cs
@@ -5,7 +5,7 @@ using Orchard.ContentManagement.FieldStorage;
 namespace Coevery.Fields.Fields {
     public class CoeveryTextField : ContentField {
         public string Value {
-            get { return Storage.Get<string>(DisplayName); }
+            get { return Storage.Get<string>(Name); }
             set { Storage.Set(value ?? String.Empty); }
         }
     }
