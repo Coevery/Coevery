f8c4f64ccb350349eb40e1c4aadfa3fe6d3444d6
Zhang Junnan <zhangjn0430@gmail.com>
2013-07-03 09:34:51 +0800

    Commit the first navigation zone
    --HG--
    branch : 1.x

--- src/Orchard.Web/Themes/Mooncake/Scripts/js/sidebar.js
+++ src/Orchard.Web/Themes/Mooncake/Scripts/js/sidebar.js
@@ -1,11 +1,11 @@
 ﻿;(function($, window) {
     $(function() {
         var sidebar = $("#sidebar");
-        sidebar.css("height", $(window).height() - 153);
+        sidebar.css("height", $(window).height() - 114);
         $('#sidebar').perfectScrollbar();
 
         $(window).resize(function() {
-            sidebar.css("height", $(window).height() - 153);
+            sidebar.css("height", $(window).height() - 114);
             $('#Demo').perfectScrollbar('update');
         });
     });
