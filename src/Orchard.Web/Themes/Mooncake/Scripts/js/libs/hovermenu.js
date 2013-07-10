2122134c25047b27c79eafd67d22412c8c0067ea
llb <llb@novasoftware.cn>
2013-07-10 15:10:52 +0800

    commit max width 481px
    --HG--
    branch : 1.x

--- src/Orchard.Web/Themes/Mooncake/Scripts/js/libs/hovermenu.js
+++ src/Orchard.Web/Themes/Mooncake/Scripts/js/libs/hovermenu.js
@@ -9,5 +9,12 @@
    .on('mouseout.dropdown-menu', '.ngGrid .ngRow', function (e) {
        $(this).find('.row-actions').addClass('hide');
    });
-
+    $(document)
+        .on("click.navmenu", '#wrapper #sidebar #navigation ul.menu > li', function (e) {
+            var navToggle = $('#header #header-right #nav-toggle');
+            if (navToggle.css('display') != 'block') return;
+            var navigation = $('#wrapper #sidebar #navigation');
+            navigation.removeClass('in');
+            navigation.css('height', 0);
+        });
 })(window.jQuery);
