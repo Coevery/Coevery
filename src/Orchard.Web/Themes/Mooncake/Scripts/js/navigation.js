29fea76a8867b951f999ef569fb723da91f1e03f
cao xiangkun <caoxiangkun@gmail.com>
2013-07-11 13:05:43 +0800

    rename responsive design
    --HG--
    branch : 1.x
    rename : src/Orchard.Web/Themes/Mooncake/Styles/sitewidget.css => src/Orchard.Web/Themes/Mooncake/Styles/layout-responsive.css

--- src/Orchard.Web/Themes/Mooncake/Scripts/js/navigation.js
+++ src/Orchard.Web/Themes/Mooncake/Scripts/js/navigation.js
@@ -1,13 +1,5 @@
-﻿;(function($, window) {
+﻿;(function($) {
     $(function() {
-        var nav = $("#navigation");
-        //nav.css("height", $(window).height() - $("#nav-selector").outerHeight() - 110);
         $('#navigation').perfectScrollbar();
-
-        //$(window).resize(function() {
-        //    nav.css("height", $(window).height() - $("#nav-selector").outerHeight() - 110);
-        //    $('#navigation').perfectScrollbar('update');
-        //    $('#navigation').perfectScrollbar('destroy');
-        //});
     });
-})(jQuery, window);
+})(jQuery);
