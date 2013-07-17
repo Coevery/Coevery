dbcbcd16d66e1c3b6c59413709299c1716122d4b
cao xiangkun <caoxiangkun@gmail.com>
2013-07-17 11:07:09 +0800

    Fix compile error
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Orchard.CulturePicker/Services/LocalizableMainMenuNavigationFilter.cs
+++ src/Orchard.Web/Modules/Orchard.CulturePicker/Services/LocalizableMainMenuNavigationFilter.cs
@@ -8,32 +8,32 @@ using Orchard.Localization.Services;
 using Orchard.UI.Navigation;
 
 namespace Orchard.CulturePicker.Services {
-    [UsedImplicitly]
-    [OrchardFeature("Orchard.CulturePicker.MainMenu")]
-    public class LocalizableMainMenuNavigationFilter : INavigationFilter {
-        private readonly ICultureManager _cultureManager;
-        private readonly IWorkContextAccessor _workContextAccessor;
+    //[UsedImplicitly]
+    //[OrchardFeature("Orchard.CulturePicker.MainMenu")]
+    //public class LocalizableMainMenuNavigationFilter : INavigationFilter {
+    //    private readonly ICultureManager _cultureManager;
+    //    private readonly IWorkContextAccessor _workContextAccessor;
 
-        public LocalizableMainMenuNavigationFilter(ICultureManager cultureManager, IWorkContextAccessor workContextAccessor) {
-            _cultureManager = cultureManager;
-            _workContextAccessor = workContextAccessor;
-        }
+    //    public LocalizableMainMenuNavigationFilter(ICultureManager cultureManager, IWorkContextAccessor workContextAccessor) {
+    //        _cultureManager = cultureManager;
+    //        _workContextAccessor = workContextAccessor;
+    //    }
 
-        #region INavigationFilter Members
+    //    #region INavigationFilter Members
 
-        public IEnumerable<MenuItem> Filter(IEnumerable<MenuItem> menuItems) {
-            string currentCulture = _cultureManager.GetCurrentCulture(_workContextAccessor.GetContext().HttpContext);
-            foreach (MenuItem menuItem in menuItems) {
-                ILocalizableAspect localizationPart = menuItem.Content.Is<ContentMenuItemPart>()
-                                                          ? menuItem.Content.As<ContentMenuItemPart>().Content.As<ILocalizableAspect>()
-                                                          : menuItem.Content.As<ILocalizableAspect>();
-                if (localizationPart == null
-                    || localizationPart.Culture == null
-                    || localizationPart.Culture == currentCulture)
-                    yield return menuItem;
-            }
-        }
+    //    public IEnumerable<MenuItem> Filter(IEnumerable<MenuItem> menuItems) {
+    //        string currentCulture = _cultureManager.GetCurrentCulture(_workContextAccessor.GetContext().HttpContext);
+    //        foreach (MenuItem menuItem in menuItems) {
+    //            ILocalizableAspect localizationPart = menuItem.Content.Is<ContentMenuItemPart>()
+    //                                                      ? menuItem.Content.As<ContentMenuItemPart>().Content.As<ILocalizableAspect>()
+    //                                                      : menuItem.Content.As<ILocalizableAspect>();
+    //            if (localizationPart == null
+    //                || localizationPart.Culture == null
+    //                || localizationPart.Culture == currentCulture)
+    //                yield return menuItem;
+    //        }
+    //    }
 
-        #endregion
-    }
+    //    #endregion
+    //}
 }
