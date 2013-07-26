3c9d549406b19d21ba9ba36eea416d570d4006f4
Zhang Junnan <zhangjn0430@gmail.com>
2013-07-26 15:02:37 +0800

    fix the can't navigate relationship page bug
    --HG--
    branch : 1.x

---
+++ src/Orchard.Web/Modules/Coevery.Entities/Scripts/controllers/manytomanydetailcontroller.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+define(['core/app/detourService'], function (detour) {
+    detour.registerController([
+        'EditManyToManyCtrl',
+        ['$scope', 'logger', '$detour', '$stateParams',
+            function ($scope, logger, $detour, $stateParams) {
+                $scope.showPrimaryList = true;
+                $scope.showRelatedList = true;
+                $scope.exit = function () {
+                    $detour.transitionTo('EntityDetail.Relationships', { Id: $stateParams.EntityName });
+                };
+            }]
+    ]);
+});
---
+++ src/Orchard.Web/Modules/Coevery.Entities/Scripts/controllers/onetomanydetailcontroller.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+define(['core/app/detourService'], function (detour) {
+    detour.registerController([
+        'EditOneToManyCtrl',
+        ['$scope', 'logger', '$detour', '$stateParams',
+            function ($scope, logger, $detour, $stateParams) {
+                $scope.recordDeleteBehavior = 1;
+                $scope.showRelatedList = true;
+                $scope.$watch('required', function (newValue) {
+                    if (newValue && $scope.recordDeleteBehavior == 1) {
+                        $scope.recordDeleteBehavior = 2;
+                    }
+                });
+                $scope.exit = function () {
+                    $detour.transitionTo('EntityDetail.Relationships', { Id: $stateParams.EntityName });
+                };
+            }]
+    ]);
+});
