f37ef1a10bc518c1cc2465576084c3b92ee19846
zhanc <zhanc@novasoftware.cn>
2013-07-29 09:25:40 +0800

    IconClass
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Core/Models/ModuleMenuItemPartRecord.cs
+++ src/Orchard.Web/Modules/Coevery.Core/Models/ModuleMenuItemPartRecord.cs
@@ -4,5 +4,6 @@ using Orchard.Core.Settings.Metadata.Records;
 namespace Coevery.Core.Models {
     public class ModuleMenuItemPartRecord : ContentPartRecord {
         public virtual ContentTypeDefinitionRecord ContentTypeDefinitionRecord { get; set; }
+        public virtual string IconClass { get; set; }
     }
 }
