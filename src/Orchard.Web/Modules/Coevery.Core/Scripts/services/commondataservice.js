3e5db3f9c163dbd3aea41bafcc73e9443caef3bf
llb <llb@novasoftware.cn>
2013-07-30 14:55:32 +0800

    commit ngGrid paging
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Core/Scripts/services/commondataservice.js
+++ src/Orchard.Web/Modules/Coevery.Core/Scripts/services/commondataservice.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-define(['core/app/couchPotatoService'], function(couchPotato) {
+define(['core/app/couchPotatoService'], function (couchPotato) {
 
     //This handles retrieving data and is used by controllers. 3 options (server, factory, provider) with 
     //each doing the same thing just structuring the functions/data differently.
@@ -8,10 +8,12 @@ define(['core/app/couchPotatoService'], function(couchPotato) {
     //Although this is an AngularJS factory I prefer the term "service" for data operations
     couchPotato.registerFactory([
         'commonDataService',
-        ['$rootScope', '$resource', function($rootScope, $resource) {
-            return $resource('api/CoeveryCore/Common/:contentType:contentId',
+        ['$rootScope', '$resource', function ($rootScope, $resource) {
+            return $resource('api/CoeveryCore/Common/:contentType:contentId:PageSize:Page',
                 { contentType: '@contentType' },
                 { contentId: '@ContentId' },
+                { pageSize: '@PageSize' },
+                { page: '@Page' },
                 { update: { method: 'PUT' } });
         }]
     ]);
