c2ce000af3dd4957d5692f6bb020f1435c4375b8
cao xiangkun <caoxiangkun@gmail.com>
2013-08-06 16:49:00 +0800

    refactor system admin theme
    --HG--
    branch : 1.x
    extra : rebase_source : bd07d71571ee97df9d5c5a0c5f54f9b6a0714ab8

---
+++ src/Orchard.Web/Modules/Coevery.Core/CoreShapes.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Routing;
+using Orchard;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Descriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Mvc;
+
+namespace Coevery.Core {
+    public class CoreShapes : IShapeTableProvider {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CoreShapes(IHttpContextAccessor httpContextAccessor) {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Discover(ShapeTableBuilder builder) {
+            builder.Describe("Layout")
+                   .OnDisplaying(displaying => {
+                       IShape layout = displaying.Shape;
+                       var httpContext = _httpContextAccessor.Current();
+                       var routeValues = httpContext.Request.RequestContext.RouteData.Values;
+                       var controller = (string)routeValues["controller"];
+                       if (controller == "SystemAdmin")
+                       layout.Metadata.Alternates.Add("Layout__" + controller);
+                   });
+
+            builder.Describe("Menu")
+                   .OnDisplaying(displaying => {
+                       IShape layout = displaying.Shape;
+                       var httpContext = _httpContextAccessor.Current();
+                       var routeValues = httpContext.Request.RequestContext.RouteData.Values;
+                       var controller = (string)routeValues["controller"];
+                       if (controller == "SystemAdmin")
+                           layout.Metadata.Alternates.Add("Layout__" + controller);
+                   });
+        }
+    }
+}
