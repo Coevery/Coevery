1541f6ca939ec3f1fe4b22ca48f1515d6296c99a
huangbx <huangbx@novasoftware.cn>
2013-08-07 00:03:23 +0800

    relationship-remove virtual
    --HG--
    branch : 1.x
    extra : rebase_source : a36386b0f838a346f453f241b1e7a2297dfaa78f

--- src/Orchard.Web/Modules/Coevery.Relationship/Models/ManyToManyRelationshipPart.cs
+++ src/Orchard.Web/Modules/Coevery.Relationship/Models/ManyToManyRelationshipPart.cs
@@ -6,42 +6,42 @@ namespace Coevery.Relationship.Models
     public class ManyToManyRelationshipPart : ContentPart<ManyToManyRelationshipRecord>
     {
         [Required]
-        public virtual int Id
+        public int Id
         {
             get { return Record.Id; }
             set { Record.Id = value; }
         }
 
         [Required]
-        public virtual RelationshipRecord Relationship
+        public RelationshipRecord Relationship
         {
             get { return Record.Relationship; }
             set { Record.Relationship = value; }
         }
 
         [Required]
-        public virtual string RelatedListLabel
+        public string RelatedListLabel
         {
             get { return Record.RelatedListLabel; }
             set { Record.RelatedListLabel = value; }
         }
 
         [Required]
-        public virtual bool ShowRelatedList
+        public bool ShowRelatedList
         {
             get { return Record.ShowRelatedList; }
             set { Record.ShowRelatedList = value; }
         }
 
         [Required]
-        public virtual string PrimaryListLabel
+        public string PrimaryListLabel
         {
             get { return Record.PrimaryListLabel; }
             set { Record.PrimaryListLabel = value; }
         }
 
         [Required]
-        public virtual bool ShowPrimaryList
+        public bool ShowPrimaryList
         {
             get { return Record.ShowPrimaryList; }
             set { Record.ShowPrimaryList = value; }
