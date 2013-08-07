e4a4640406c28d33188ed6703896f832b2c923dd
huangbx <huangbx@novasoftware.cn>
2013-08-07 11:09:25 +0800

    Theme refactor-two layout
    --HG--
    branch : 1.x

--- src/Orchard.Web/Modules/Coevery.Core/Admin/SystemAdminThemeSelector.cs
+++ src/Orchard.Web/Modules/Coevery.Core/Admin/SystemAdminThemeSelector.cs
@@ -5,7 +5,8 @@ namespace Coevery.Core.Admin {
     public class SystemAdminThemeSelector : IThemeSelector {
         public ThemeSelectorResult GetTheme(RequestContext context) {
             if (SystemAdminFilter.IsApplied(context)) {
-                return new ThemeSelectorResult { Priority = 100, ThemeName = "MooncakeAdmin" };
+                return new ThemeSelectorResult { Priority = 100, ThemeName = "Mooncake" };
+                //return new ThemeSelectorResult { Priority = 100, ThemeName = "MooncakeAdmin" };
             }
             return null;
         }
