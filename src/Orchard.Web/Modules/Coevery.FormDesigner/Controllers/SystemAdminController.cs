b55961c6d310adcd3763198d01f327b893fec7d8
Zhang Junnan <zhangjn0430@gmail.com>
2013-09-03 18:44:25 +0800

    modify the many to many relationship label in form designer
    --HG--
    branch : 1.x
    extra : rebase_source : f9bdc6d810a379feb96d97ddb048e4753a784b8a

--- src/Orchard.Web/Modules/Coevery.FormDesigner/Controllers/SystemAdminController.cs
+++ src/Orchard.Web/Modules/Coevery.FormDesigner/Controllers/SystemAdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Web.Http;
 using System.Web.Mvc;
 using Coevery.Core.Admin;
+using Coevery.FormDesigner.ViewModels;
 using Orchard;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.MetaData;
@@ -37,30 +39,35 @@ namespace Coevery.FormDesigner.Controllers {
             dynamic model = _contentManager.BuildEditor(contentItem);
             var contentTypeDefinition = contentItem.TypeDefinition;
             string layout = contentTypeDefinition.Settings.ContainsKey("Layout")
-                                ? contentTypeDefinition.Settings["Layout"]
-                                : string.Empty;
+                ? contentTypeDefinition.Settings["Layout"]
+                : string.Empty;
             layout = string.IsNullOrEmpty(layout)
-                         ? "<fd-section section-columns=\"1\" section-columns-width=\"6:6\" section-title=\"Sample Title\"><fd-row><fd-column></fd-column></fd-row></fd-section>"
-                         : layout;
+                ? "<fd-section section-columns=\"1\" section-columns-width=\"6:6\" section-title=\"Sample Title\"><fd-row><fd-column></fd-column></fd-row></fd-section>"
+                : layout;
             var viewModel = Services.New.ViewModel();
             viewModel.Layout = layout;
-            viewModel.Fields = _contentDefinitionManager.GetPartDefinition(id).Fields;
             viewModel.DisplayName = contentItem.TypeDefinition.DisplayName;
             var templates = new List<dynamic>();
-            var parts = new List<string>();
+            var fields = _contentDefinitionManager.GetPartDefinition(id).Fields
+                .Select(x => new FieldViewModel {DisplayName = x.DisplayName, Name = x.Name})
+                .ToList();
             foreach (var item in model.Content.Items) {
                 if (item.TemplateName != null && item.TemplateName.StartsWith("Fields/")) {
                     templates.Add(item);
                 }
                 else if (item.TemplateName == "Parts/Relationship.Edit") {
                     templates.Add(item);
-                    parts.Add(item.ContentPart.GetType().Name);
+                    var name = item.ContentPart.GetType().Name;
+                    fields.Add(new FieldViewModel {
+                        Name = name,
+                        DisplayName = _contentDefinitionManager.GetPartDefinition(name).Settings["DisplayName"]
+                    });
                 }
             }
             viewModel.Templates = templates;
-            viewModel.Parts = parts;
+            viewModel.Fields = fields;
 
-            return View((object)viewModel);
+            return View((object) viewModel);
         }
     }
 }
