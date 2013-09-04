1094116363bf0e2c4cdbeb4a5afe6a19069b0ade
zxy54 <zxy54@Outlook.com>
2013-09-04 15:35:07 +0800

    fixed entity detail delete button
    --HG--
    branch : 1.x
    extra : rebase_source : 43ea3b0534112f511b4cc8ff7c0f545cac5e84a3

--- src/Orchard.Web/Modules/Coevery.Entities/Scripts/controllers/detailcontroller.js
+++ src/Orchard.Web/Modules/Coevery.Entities/Scripts/controllers/detailcontroller.js
@@ -1,10 +1,10 @@
 ﻿'use strict';
 
-define(['core/app/detourService'], function (detour) {
+define(['core/app/detourService', 'Modules/Coevery.Entities/Scripts/services/entitydataservice'], function (detour) {
     detour.registerController([
         'EntityDetailCtrl',
-        ['$scope', 'logger', '$detour', '$stateParams',
-            function ($scope, logger, $detour, $stateParams) {
+        ['$scope', 'logger', '$detour', '$stateParams','entityDataService',
+            function ($scope, logger, $detour, $stateParams, entityDataService) {
 
                 $scope.exit = function() {
                     $detour.transitionTo('EntityList');
@@ -12,6 +12,15 @@ define(['core/app/detourService'], function (detour) {
                 
                 $scope.edit = function () {
                     $detour.transitionTo('EntityEdit', { Id: $stateParams.Id });
+                };
+
+                $scope.delete = function() {
+                    entityDataService.delete({ name: $stateParams.Id }, function () {
+                        $detour.transitionTo('EntityList');
+                        logger.success("Delete the entity successful.");
+                    }, function (reason) {
+                        logger.error("Failed to delete the entity." + reason);
+                    });
                 };
                 
                 $scope.formDesigner = function() {
