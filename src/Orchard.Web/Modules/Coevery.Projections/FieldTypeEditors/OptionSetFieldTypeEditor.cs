18d3f018ed6a5aa41228bee1528bb7b8301a9f60
zhouxianghao <hizhouxianghao@163.com>
2013-09-19 16:20:49 +0800

    add optionset filter

--- src/Orchard.Web/Modules/Coevery.Projections/FieldTypeEditors/OptionSetFieldTypeEditor.cs
+++ src/Orchard.Web/Modules/Coevery.Projections/FieldTypeEditors/OptionSetFieldTypeEditor.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using Coevery.OptionSet.Models;
 using Coevery.Projections.FilterEditors.Forms;
 using Orchard.ContentManagement;
 using Orchard.Localization;
+
 
 namespace Coevery.Projections.FieldTypeEditors {
     public class OptionSetFieldTypeEditor : ILogicFieldTypeEditor {
@@ -22,7 +25,35 @@ namespace Coevery.Projections.FieldTypeEditors {
         public void ApplyFilter(dynamic context) {
             var op = (OptionSetOperator) Enum.Parse(typeof (OptionSetOperator), (string) context.State.Operator);
             string value = context.State.Value;
+            if (value == null) return;
             string[] valueArrStr = value.Split(new string[] {"&"}, StringSplitOptions.RemoveEmptyEntries);
+            List<int> valueArr = new List<int>();
+            foreach (var valueItem in valueArrStr)
+            {
+                valueArr.Add(int.Parse(valueItem));
+            }
+            switch (op)
+            {
+                case OptionSetOperator.MatchesAny:
+                        Action<IAliasFactory> selectorAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
+                        Action<IHqlExpressionFactory> filterAny = x => x.InG("Id", valueArr);
+                        context.Query.Where(selectorAny, filterAny);
+                    break;
+                case OptionSetOperator.MatchesAll:
+                    foreach (var id in valueArr)
+                    {
+                        Action<IAliasFactory> selectorAll =
+                            alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opit" + id);
+                        Action<IHqlExpressionFactory> filterAll = x => x.Eq("OptionItemRecord.Id", id);
+                        context.Query.Where(selectorAll, filterAll);
+                    }
+                    break;
+                case OptionSetOperator.NotMatchesAny:
+                        Action<IAliasFactory> selectorNotAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
+                        Action<IHqlExpressionFactory> filterNotAny = x => x.Not(y => y.InG("Id", valueArr));
+                        context.Query.Where(selectorNotAny, filterNotAny);
+                    break;
+            }
         }
 
         public bool CanHandle(Type storageType) {
