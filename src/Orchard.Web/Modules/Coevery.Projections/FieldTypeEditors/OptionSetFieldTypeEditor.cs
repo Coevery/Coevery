690268515ee9a73a1e74533e200f87ca5ea4f86e
Zhang Junnan <zhangjn0430@gmail.com>
2013-09-19 17:03:13 +0800

    Refactor the options set type filter

--- src/Orchard.Web/Modules/Coevery.Projections/FieldTypeEditors/OptionSetFieldTypeEditor.cs
+++ src/Orchard.Web/Modules/Coevery.Projections/FieldTypeEditors/OptionSetFieldTypeEditor.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using Coevery.OptionSet.Models;
 using Coevery.Projections.FilterEditors.Forms;
 using Orchard.ContentManagement;
 using Orchard.Localization;
-
 
 namespace Coevery.Projections.FieldTypeEditors {
     public class OptionSetFieldTypeEditor : ILogicFieldTypeEditor {
@@ -25,33 +24,29 @@ namespace Coevery.Projections.FieldTypeEditors {
         public void ApplyFilter(dynamic context) {
             var op = (OptionSetOperator) Enum.Parse(typeof (OptionSetOperator), (string) context.State.Operator);
             string value = context.State.Value;
-            if (value == null) return;
-            string[] valueArrStr = value.Split(new string[] {"&"}, StringSplitOptions.RemoveEmptyEntries);
-            List<int> valueArr = new List<int>();
-            foreach (var valueItem in valueArrStr)
-            {
-                valueArr.Add(int.Parse(valueItem));
+            if (value == null) {
+                return;
             }
-            switch (op)
-            {
+            var valueArr = value.Split(new[] {"&"}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            switch (op) {
                 case OptionSetOperator.MatchesAny:
-                        Action<IAliasFactory> selectorAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
-                        Action<IHqlExpressionFactory> filterAny = x => x.InG("Id", valueArr);
-                        context.Query.Where(selectorAny, filterAny);
+                    Action<IAliasFactory> selectorAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
+                    Action<IHqlExpressionFactory> filterAny = x => x.InG("Id", valueArr);
+                    context.Query.Where(selectorAny, filterAny);
                     break;
                 case OptionSetOperator.MatchesAll:
-                    foreach (var id in valueArr)
-                    {
+                    foreach (var id in valueArr) {
+                        var optionId = id;
                         Action<IAliasFactory> selectorAll =
-                            alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opit" + id);
-                        Action<IHqlExpressionFactory> filterAll = x => x.Eq("OptionItemRecord.Id", id);
+                            alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opit" + optionId);
+                        Action<IHqlExpressionFactory> filterAll = x => x.Eq("OptionItemRecord.Id", optionId);
                         context.Query.Where(selectorAll, filterAll);
                     }
                     break;
                 case OptionSetOperator.NotMatchesAny:
-                        Action<IAliasFactory> selectorNotAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
-                        Action<IHqlExpressionFactory> filterNotAny = x => x.Not(y => y.InG("Id", valueArr));
-                        context.Query.Where(selectorNotAny, filterNotAny);
+                    Action<IAliasFactory> selectorNotAny = alias => alias.ContentPartRecord<OptionItemContainerPartRecord>().Property("OptionItems", "opits").Property("OptionItemRecord", "opcpr");
+                    Action<IHqlExpressionFactory> filterNotAny = x => x.Not(y => y.InG("Id", valueArr));
+                    context.Query.Where(selectorNotAny, filterNotAny);
                     break;
             }
         }
