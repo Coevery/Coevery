f06c7b05ffec2ecd80ab6a785c1439baca3402d9
Zhang Junnan <zhangjn0430@gmail.com>
2013-09-26 12:02:23 +0800

    Add metadata version manage

---
+++ src/Orchard.Web/Modules/Coevery.Entities/Drivers/EntityMetadataPartDriver.cs
@@ -0,0 +1,7 @@
+﻿using Coevery.Entities.Models;
+using Orchard.ContentManagement.Drivers;
+
+namespace Coevery.Entities.Drivers {
+    public class EntityMetadataPartDriver : ContentPartDriver<EntityMetadataPart> {
+    }
+}
---
+++ src/Orchard.Web/Modules/Coevery.Entities/Handlers/EntityMetadataPartHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Coevery.Entities.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace Coevery.Entities.Handlers {
+    public class EntityMetadataPartHandler : ContentHandler {
+        private readonly IRepository<FieldMetadataRecord> _fieldMetadataRepository;
+
+        public EntityMetadataPartHandler(
+            IRepository<EntityMetadataRecord> repository,
+            IRepository<FieldMetadataRecord> fieldMetadataRepository) {
+            _fieldMetadataRepository = fieldMetadataRepository;
+
+            Filters.Add(StorageFilter.For(repository));
+
+            OnPublished<EntityMetadataPart>((context, part) => { });
+        }
+
+        protected override void Versioning(VersionContentContext context) {
+            var building = context.BuildingContentItem.As<EntityMetadataPart>();
+            var existing = context.ExistingContentItem.As<EntityMetadataPart>();
+
+            building.Record.FieldMetadataRecords = new List<FieldMetadataRecord>();
+            foreach (var record in existing.Record.FieldMetadataRecords) {
+                var newRecord = new FieldMetadataRecord();
+                _fieldMetadataRepository.Copy(record, newRecord);
+                newRecord.OriginalId = record.Id;
+                _fieldMetadataRepository.Create(newRecord);
+                building.Record.FieldMetadataRecords.Add(newRecord);
+            }
+        }
+    }
+}
