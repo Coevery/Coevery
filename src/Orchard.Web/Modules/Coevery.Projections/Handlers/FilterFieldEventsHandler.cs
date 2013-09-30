8830e83c93d2e42a6f5caae72abf1b7fab8e3bf6
Zhang Junnan <zhangjn0430@gmail.com>
2013-09-30 17:02:03 +0800

    Auto delete filter when delete field

---
+++ src/Orchard.Web/Modules/Coevery.Projections/Handlers/FilterFieldEventsHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Coevery.Entities.Events;
+using Coevery.Projections.Models;
+using Orchard.Data;
+
+namespace Coevery.Projections.Handlers {
+    public class FilterFieldEventsHandler : IFieldEvents {
+        private readonly IRepository<EntityFilterRecord> _entityFilterRepository;
+
+        public FilterFieldEventsHandler(IRepository<EntityFilterRecord> entityFilterRepository) {
+            _entityFilterRepository = entityFilterRepository;
+        }
+
+        public void OnCreated(string entityName, string fieldName, bool isInLayout) {}
+
+        public void OnDeleting(string entityName, string fieldName) {
+            var entityFilterRecords = _entityFilterRepository.Table.Where(x => x.EntityName == entityName).ToList();
+            foreach (var entityFilterRecord in entityFilterRecords) {
+                string type = string.Format("{0}.{1}.", entityName, fieldName);
+                var filters = entityFilterRecord.FilterGroupRecord.Filters.Where(x => x.Type == type).ToList();
+                filters.ForEach(record => entityFilterRecord.FilterGroupRecord.Filters.Remove(record));
+                if (entityFilterRecord.FilterGroupRecord.Filters.Count == 0) {
+                    _entityFilterRepository.Delete(entityFilterRecord);
+                }
+                else {
+                    _entityFilterRepository.Update(entityFilterRecord);
+                }
+            }
+        }
+    }
+}
