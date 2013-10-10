3bd0f2647a603957d63c5c7a8b47c786c37d5292
cao xiangkun <caoxiangkun@gmail.com>
2013-10-10 16:10:02 +0800

    update assembly version to 1.0.1

--- src/Orchard.Web/Modules/Coevery.Core/Properties/AssemblyInfo.cs
+++ src/Orchard.Web/Modules/Coevery.Core/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha")]
