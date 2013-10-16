f0fa49daad6d0dd020999c0c8620d3a4020f745e
zxy54 <zxy54@Outlook.com>
2013-10-16 14:44:48 +0800

    Modified view

--- src/Orchard.Web/Modules/Coevery.Core/Scripts/app/adminapp.js
+++ src/Orchard.Web/Modules/Coevery.Core/Scripts/app/adminapp.js
@@ -45,7 +45,7 @@
 
             $rootScope.cellLinkTemplate = function (cellvalue, options, rowObject) {
                 var template = '<div class="gridCellText">' +
-                    '<section class="row-actions hide">' +
+                    '<section class="row-actions">' +
                     '<span class="icon-edit" data-ng-click="edit(\'{0}\')" title="Edit"></span>' +
                     '<span class="icon-remove" co-delete-button confirm-message="You really want to delete this row?" ' +
                     'delete-action="delete(\'{0}\')" title="Delete"></span>{2}</section>' +
