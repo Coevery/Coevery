6ae978d8b1432cd90179fcbd8d05557477594117
zhouxianghao <hizhouxianghao@163.com>
2013-11-07 22:25:26 +0800

    add spin to save button

--- src/Coevery.Web/Modules/Coevery.AngularJS/Scripts/spin.js
+++ src/Coevery.Web/Modules/Coevery.AngularJS/Scripts/spin.js
@@ -1,1 +1,347 @@
-﻿(function (t, e) { if (typeof exports == "object") module.exports = e(); else if (typeof define == "function" && define.amd) define(e); else t.Spinner = e() })(this, function () { "use strict"; var t = ["webkit", "Moz", "ms", "O"], e = {}, i; function o(t, e) { var i = document.createElement(t || "div"), o; for (o in e) i[o] = e[o]; return i } function n(t) { for (var e = 1, i = arguments.length; e < i; e++) t.appendChild(arguments[e]); return t } var r = function () { var t = o("style", { type: "text/css" }); n(document.getElementsByTagName("head")[0], t); return t.sheet || t.styleSheet }(); function s(t, o, n, s) { var a = ["opacity", o, ~~(t * 100), n, s].join("-"), f = .01 + n / s * 100, l = Math.max(1 - (1 - t) / o * (100 - f), t), u = i.substring(0, i.indexOf("Animation")).toLowerCase(), d = u && "-" + u + "-" || ""; if (!e[a]) { r.insertRule("@" + d + "keyframes " + a + "{" + "0%{opacity:" + l + "}" + f + "%{opacity:" + t + "}" + (f + .01) + "%{opacity:1}" + (f + o) % 100 + "%{opacity:" + t + "}" + "100%{opacity:" + l + "}" + "}", r.cssRules.length); e[a] = 1 } return a } function a(e, i) { var o = e.style, n, r; i = i.charAt(0).toUpperCase() + i.slice(1); for (r = 0; r < t.length; r++) { n = t[r] + i; if (o[n] !== undefined) return n } if (o[i] !== undefined) return i } function f(t, e) { for (var i in e) t.style[a(t, i) || i] = e[i]; return t } function l(t) { for (var e = 1; e < arguments.length; e++) { var i = arguments[e]; for (var o in i) if (t[o] === undefined) t[o] = i[o] } return t } function u(t) { var e = { x: t.offsetLeft, y: t.offsetTop }; while (t = t.offsetParent) e.x += t.offsetLeft, e.y += t.offsetTop; return e } function d(t, e) { return typeof t == "string" ? t : t[e % t.length] } var p = { lines: 12, length: 7, width: 5, radius: 10, rotate: 0, corners: 1, color: "#000", direction: 1, speed: 1, trail: 100, opacity: 1 / 4, fps: 20, zIndex: 2e9, className: "spinner", top: "auto", left: "auto", position: "relative" }; function c(t) { if (typeof this == "undefined") return new c(t); this.opts = l(t || {}, c.defaults, p) } c.defaults = {}; l(c.prototype, { spin: function (t) { this.stop(); var e = this, n = e.opts, r = e.el = f(o(0, { className: n.className }), { position: n.position, width: 0, zIndex: n.zIndex }), s = n.radius + n.length + n.width, a, l; if (t) { t.insertBefore(r, t.firstChild || null); l = u(t); a = u(r); f(r, { left: (n.left == "auto" ? l.x - a.x + (t.offsetWidth >> 1) : parseInt(n.left, 10) + s) + "px", top: (n.top == "auto" ? l.y - a.y + (t.offsetHeight >> 1) : parseInt(n.top, 10) + s) + "px" }) } r.setAttribute("role", "progressbar"); e.lines(r, e.opts); if (!i) { var d = 0, p = (n.lines - 1) * (1 - n.direction) / 2, c, h = n.fps, m = h / n.speed, y = (1 - n.opacity) / (m * n.trail / 100), g = m / n.lines; (function v() { d++; for (var t = 0; t < n.lines; t++) { c = Math.max(1 - (d + (n.lines - t) * g) % m * y, n.opacity); e.opacity(r, t * n.direction + p, c, n) } e.timeout = e.el && setTimeout(v, ~~(1e3 / h)) })() } return e }, stop: function () { var t = this.el; if (t) { clearTimeout(this.timeout); if (t.parentNode) t.parentNode.removeChild(t); this.el = undefined } return this }, lines: function (t, e) { var r = 0, a = (e.lines - 1) * (1 - e.direction) / 2, l; function u(t, i) { return f(o(), { position: "absolute", width: e.length + e.width + "px", height: e.width + "px", background: t, boxShadow: i, transformOrigin: "left", transform: "rotate(" + ~~(360 / e.lines * r + e.rotate) + "deg) translate(" + e.radius + "px" + ",0)", borderRadius: (e.corners * e.width >> 1) + "px" }) } for (; r < e.lines; r++) { l = f(o(), { position: "absolute", top: 1 + ~(e.width / 2) + "px", transform: e.hwaccel ? "translate3d(0,0,0)" : "", opacity: e.opacity, animation: i && s(e.opacity, e.trail, a + r * e.direction, e.lines) + " " + 1 / e.speed + "s linear infinite" }); if (e.shadow) n(l, f(u("#000", "0 0 4px " + "#000"), { top: 2 + "px" })); n(t, n(l, u(d(e.color, r), "0 0 1px rgba(0,0,0,.1)"))) } return t }, opacity: function (t, e, i) { if (e < t.childNodes.length) t.childNodes[e].style.opacity = i } }); function h() { function t(t, e) { return o("<" + t + ' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">', e) } r.addRule(".spin-vml", "behavior:url(#default#VML)"); c.prototype.lines = function (e, i) { var o = i.length + i.width, r = 2 * o; function s() { return f(t("group", { coordsize: r + " " + r, coordorigin: -o + " " + -o }), { width: r, height: r }) } var a = -(i.width + i.length) * 2 + "px", l = f(s(), { position: "absolute", top: a, left: a }), u; function p(e, r, a) { n(l, n(f(s(), { rotation: 360 / i.lines * e + "deg", left: ~~r }), n(f(t("roundrect", { arcsize: i.corners }), { width: o, height: i.width, left: i.radius, top: -i.width >> 1, filter: a }), t("fill", { color: d(i.color, e), opacity: i.opacity }), t("stroke", { opacity: 0 })))) } if (i.shadow) for (u = 1; u <= i.lines; u++) p(u, -2, "progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)"); for (u = 1; u <= i.lines; u++) p(u); return n(e, l) }; c.prototype.opacity = function (t, e, i, o) { var n = t.firstChild; o = o.shadow && o.lines || 0; if (n && e + o < n.childNodes.length) { n = n.childNodes[e + o]; n = n && n.firstChild; n = n && n.firstChild; if (n) n.opacity = i } } } var m = f(o("group"), { behavior: "url(#default#VML)" }); if (!a(m, "transform") && m.adj) h(); else i = a(m, "animation"); return c });
+﻿//fgnass.github.com/spin.js#v1.3.2
+
+/**
+ * Copyright (c) 2011-2013 Felix Gnass
+ * Licensed under the MIT license
+ */
+(function (root, factory) {
+    root.Spinner = factory()
+}
+(this, function () {
+    "use strict";
+
+    var prefixes = ['webkit', 'Moz', 'ms', 'O'] /* Vendor prefixes */
+      , animations = {} /* Animation rules keyed by their name */
+      , useCssAnimations /* Whether to use CSS animations or setTimeout */
+
+    /**
+     * Utility function to create elements. If no tag name is given,
+     * a DIV is created. Optionally properties can be passed.
+     */
+    function createEl(tag, prop) {
+        var el = document.createElement(tag || 'div')
+          , n
+
+        for (n in prop) el[n] = prop[n]
+        return el
+    }
+
+    /**
+     * Appends children and returns the parent.
+     */
+    function ins(parent /* child1, child2, ...*/) {
+        for (var i = 1, n = arguments.length; i < n; i++)
+            parent.appendChild(arguments[i])
+
+        return parent
+    }
+
+    /**
+     * Insert a new stylesheet to hold the @keyframe or VML rules.
+     */
+    var sheet = (function () {
+        var el = createEl('style', { type: 'text/css' })
+        ins(document.getElementsByTagName('head')[0], el)
+        return el.sheet || el.styleSheet
+    }())
+
+    /**
+     * Creates an opacity keyframe animation rule and returns its name.
+     * Since most mobile Webkits have timing issues with animation-delay,
+     * we create separate rules for each line/segment.
+     */
+    function addAnimation(alpha, trail, i, lines) {
+        var name = ['opacity', trail, ~~(alpha * 100), i, lines].join('-')
+          , start = 0.01 + i / lines * 100
+          , z = Math.max(1 - (1 - alpha) / trail * (100 - start), alpha)
+          , prefix = useCssAnimations.substring(0, useCssAnimations.indexOf('Animation')).toLowerCase()
+          , pre = prefix && '-' + prefix + '-' || ''
+
+        if (!animations[name]) {
+            sheet.insertRule(
+              '@' + pre + 'keyframes ' + name + '{' +
+              '0%{opacity:' + z + '}' +
+              start + '%{opacity:' + alpha + '}' +
+              (start + 0.01) + '%{opacity:1}' +
+              (start + trail) % 100 + '%{opacity:' + alpha + '}' +
+              '100%{opacity:' + z + '}' +
+              '}', sheet.cssRules.length)
+
+            animations[name] = 1
+        }
+
+        return name
+    }
+
+    /**
+     * Tries various vendor prefixes and returns the first supported property.
+     */
+    function vendor(el, prop) {
+        var s = el.style
+          , pp
+          , i
+
+        prop = prop.charAt(0).toUpperCase() + prop.slice(1)
+        for (i = 0; i < prefixes.length; i++) {
+            pp = prefixes[i] + prop
+            if (s[pp] !== undefined) return pp
+        }
+        if (s[prop] !== undefined) return prop
+    }
+
+    /**
+     * Sets multiple style properties at once.
+     */
+    function css(el, prop) {
+        for (var n in prop)
+            el.style[vendor(el, n) || n] = prop[n]
+
+        return el
+    }
+
+    /**
+     * Fills in default values.
+     */
+    function merge(obj) {
+        for (var i = 1; i < arguments.length; i++) {
+            var def = arguments[i]
+            for (var n in def)
+                if (obj[n] === undefined) obj[n] = def[n]
+        }
+        return obj
+    }
+
+    /**
+     * Returns the absolute page-offset of the given element.
+     */
+    function pos(el) {
+        var o = { x: el.offsetLeft, y: el.offsetTop }
+        while ((el = el.offsetParent))
+            o.x += el.offsetLeft, o.y += el.offsetTop
+
+        return o
+    }
+
+    /**
+     * Returns the line color from the given string or array.
+     */
+    function getColor(color, idx) {
+        return typeof color == 'string' ? color : color[idx % color.length]
+    }
+
+    // Built-in defaults
+
+    var defaults = {
+        lines: 12,            // The number of lines to draw
+        length: 7,            // The length of each line
+        width: 5,             // The line thickness
+        radius: 10,           // The radius of the inner circle
+        rotate: 0,            // Rotation offset
+        corners: 1,           // Roundness (0..1)
+        color: '#000',        // #rgb or #rrggbb
+        direction: 1,         // 1: clockwise, -1: counterclockwise
+        speed: 1,             // Rounds per second
+        trail: 100,           // Afterglow percentage
+        opacity: 1 / 4,         // Opacity of the lines
+        fps: 20,              // Frames per second when using setTimeout()
+        zIndex: 2e9,          // Use a high z-index by default
+        className: 'spinner', // CSS class to assign to the element
+        top: 'auto',          // center vertically
+        left: 'auto',         // center horizontally
+        position: 'relative'  // element position
+    }
+
+    /** The constructor */
+    function Spinner(o) {
+        if (typeof this == 'undefined') return new Spinner(o)
+        this.opts = merge(o || {}, Spinner.defaults, defaults)
+    }
+
+    // Global defaults that override the built-ins:
+    Spinner.defaults = {}
+
+    merge(Spinner.prototype, {
+
+        /**
+         * Adds the spinner to the given target element. If this instance is already
+         * spinning, it is automatically removed from its previous target b calling
+         * stop() internally.
+         */
+        spin: function (target) {
+            this.stop()
+
+            var self = this
+              , o = self.opts
+              , el = self.el = css(createEl(0, { className: o.className }), { position: o.position, width: 0, zIndex: o.zIndex })
+              , mid = o.radius + o.length + o.width
+              , ep // element position
+              , tp // target position
+
+            if (target) {
+                target.insertBefore(el, target.firstChild || null)
+                tp = pos(target)
+                ep = pos(el)
+                css(el, {
+                    left: (o.left == 'auto' ? tp.x - ep.x + (target.offsetWidth >> 1) : parseInt(o.left, 10) + mid) + 'px',
+                    top: (o.top == 'auto' ? tp.y - ep.y + (target.offsetHeight >> 1) : parseInt(o.top, 10) + mid) + 'px'
+                })
+            }
+
+            el.setAttribute('role', 'progressbar')
+            self.lines(el, self.opts)
+
+            if (!useCssAnimations) {
+                // No CSS animation support, use setTimeout() instead
+                var i = 0
+                  , start = (o.lines - 1) * (1 - o.direction) / 2
+                  , alpha
+                  , fps = o.fps
+                  , f = fps / o.speed
+                  , ostep = (1 - o.opacity) / (f * o.trail / 100)
+                  , astep = f / o.lines
+
+                ; (function anim() {
+                    i++;
+                    for (var j = 0; j < o.lines; j++) {
+                        alpha = Math.max(1 - (i + (o.lines - j) * astep) % f * ostep, o.opacity)
+
+                        self.opacity(el, j * o.direction + start, alpha, o)
+                    }
+                    self.timeout = self.el && setTimeout(anim, ~~(1000 / fps))
+                })()
+            }
+            return self
+        },
+
+        /**
+         * Stops and removes the Spinner.
+         */
+        stop: function () {
+            var el = this.el
+            if (el) {
+                clearTimeout(this.timeout)
+                if (el.parentNode) el.parentNode.removeChild(el)
+                this.el = undefined
+            }
+            return this
+        },
+
+        /**
+         * Internal method that draws the individual lines. Will be overwritten
+         * in VML fallback mode below.
+         */
+        lines: function (el, o) {
+            var i = 0
+              , start = (o.lines - 1) * (1 - o.direction) / 2
+              , seg
+
+            function fill(color, shadow) {
+                return css(createEl(), {
+                    position: 'absolute',
+                    width: (o.length + o.width) + 'px',
+                    height: o.width + 'px',
+                    background: color,
+                    boxShadow: shadow,
+                    transformOrigin: 'left',
+                    transform: 'rotate(' + ~~(360 / o.lines * i + o.rotate) + 'deg) translate(' + o.radius + 'px' + ',0)',
+                    borderRadius: (o.corners * o.width >> 1) + 'px'
+                })
+            }
+
+            for (; i < o.lines; i++) {
+                seg = css(createEl(), {
+                    position: 'absolute',
+                    top: 1 + ~(o.width / 2) + 'px',
+                    transform: o.hwaccel ? 'translate3d(0,0,0)' : '',
+                    opacity: o.opacity,
+                    animation: useCssAnimations && addAnimation(o.opacity, o.trail, start + i * o.direction, o.lines) + ' ' + 1 / o.speed + 's linear infinite'
+                })
+
+                if (o.shadow) ins(seg, css(fill('#000', '0 0 4px ' + '#000'), { top: 2 + 'px' }))
+                ins(el, ins(seg, fill(getColor(o.color, i), '0 0 1px rgba(0,0,0,.1)')))
+            }
+            return el
+        },
+
+        /**
+         * Internal method that adjusts the opacity of a single line.
+         * Will be overwritten in VML fallback mode below.
+         */
+        opacity: function (el, i, val) {
+            if (i < el.childNodes.length) el.childNodes[i].style.opacity = val
+        }
+
+    })
+
+
+    function initVML() {
+
+        /* Utility function to create a VML tag */
+        function vml(tag, attr) {
+            return createEl('<' + tag + ' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">', attr)
+        }
+
+        // No CSS transforms but VML support, add a CSS rule for VML elements:
+        sheet.addRule('.spin-vml', 'behavior:url(#default#VML)')
+
+        Spinner.prototype.lines = function (el, o) {
+            var r = o.length + o.width
+              , s = 2 * r
+
+            function grp() {
+                return css(
+                  vml('group', {
+                      coordsize: s + ' ' + s,
+                      coordorigin: -r + ' ' + -r
+                  }),
+                  { width: s, height: s }
+                )
+            }
+
+            var margin = -(o.width + o.length) * 2 + 'px'
+              , g = css(grp(), { position: 'absolute', top: margin, left: margin })
+              , i
+
+            function seg(i, dx, filter) {
+                ins(g,
+                  ins(css(grp(), { rotation: 360 / o.lines * i + 'deg', left: ~~dx }),
+                    ins(css(vml('roundrect', { arcsize: o.corners }), {
+                        width: r,
+                        height: o.width,
+                        left: o.radius,
+                        top: -o.width >> 1,
+                        filter: filter
+                    }),
+                      vml('fill', { color: getColor(o.color, i), opacity: o.opacity }),
+                      vml('stroke', { opacity: 0 }) // transparent stroke to fix color bleeding upon opacity change
+                    )
+                  )
+                )
+            }
+
+            if (o.shadow)
+                for (i = 1; i <= o.lines; i++)
+                    seg(i, -2, 'progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)')
+
+            for (i = 1; i <= o.lines; i++) seg(i)
+            return ins(el, g)
+        }
+
+        Spinner.prototype.opacity = function (el, i, val, o) {
+            var c = el.firstChild
+            o = o.shadow && o.lines || 0
+            if (c && i + o < c.childNodes.length) {
+                c = c.childNodes[i + o]; c = c && c.firstChild; c = c && c.firstChild
+                if (c) c.opacity = val
+            }
+        }
+    }
+
+    var probe = css(createEl('group'), { behavior: 'url(#default#VML)' })
+
+    if (!vendor(probe, 'transform') && probe.adj) initVML()
+    else useCssAnimations = vendor(probe, 'animation')
+
+    return Spinner
+
+}));
--- src/Coevery.Web/Modules/Coevery.Common/Scripts/directives/cobusy-directive.js
+++ src/Coevery.Web/Modules/Coevery.Common/Scripts/directives/cobusy-directive.js
@@ -2,70 +2,65 @@
 var coeverybusy=angular.module('coevery.Busy', ['ajoslin.promise-tracker']);
 
 coeverybusy.directive('cobusy', ['promiseTracker', function (promiseTracker) {
-	    return {
-	        restrict: 'A',
-	        link: function (scope, element, attrs) {
+	return {
+	    restrict: 'A',
+	    link: function (scope, element, attrs) {
 
-	            var options = scope.$eval(attrs.cobusy);
+	        var options = scope.$eval(attrs.cobusy);
 
-	            if (typeof options === 'string') {
-	                options = { tracker: options };
-	            }
-
-	            if (typeof options === 'undefined' || typeof options.tracker === 'undefined') {
-	                throw new Error('Options for cobusy directive must be provided (tracker option is required).');
-	            }
-
-	            if (!scope.$coBusyTracker) {
-	                scope.$coBusyTracker = {};
-	            }
-	            
-	            scope.$coBusyTracker[options.tracker] = promiseTracker(options.tracker);
-
-	            function toBoolean(value) {
-	                if (value && value.length !== 0) {
-	                    var v = angular.lowercase("" + value);
-	                    value = !(v == 'f' || v == '0' || v == 'false' || v == 'no' || v == 'n' || v == '[]');
-	                } else {
-	                    value = false;
-	                }
-	                return value;
-	            }
-	            
-	            //var spinner,height = element.height(),
-                //    oldWidth = element.width(),
-                //    opts = {
-                //        length: Math.round(height / 3),
-                //        radius: Math.round(height / 5),
-                //        width: Math.round(height / 10),
-                //        color: element.css("color"),
-                //        left: -5
-                //    };
-	            
-	            
-
-	            var first = true;
-	            scope.$watch('$coBusyTracker.' + options.tracker + '.active()', function (value) {
-	                //ignore first
-	                if (first) {
-	                    first = false;
-	                    return; 
-	                }
-	                if (toBoolean(value)) {
-	                    //spinner = new Spinner(opts).spin(target);
-	                    //element.width(oldWidth + oldWidth / 2);
-	                    attrs.$set('disabled', true);
-	                } else {
-	                    //if (spinner) {
-	                    //    spinner.stop();
-	                    //    element.width(oldWidth);
-	                    //}
-	                    attrs.$set('disabled', false);
-	                }
-	            });
-
+	        if (typeof options === 'string') {
+	            options = { tracker: options };
 	        }
-	    };
-	}
-    ]);
+
+	        if (typeof options === 'undefined' || typeof options.tracker === 'undefined') {
+	            throw new Error('Options for cobusy directive must be provided (tracker option is required).');
+	        }
+
+	        if (!scope.$coBusyTracker) {
+	            scope.$coBusyTracker = {};
+	        }
+	            
+	        scope.$coBusyTracker[options.tracker] = promiseTracker(options.tracker);
+
+	        function toBoolean(value) {
+	            if (value && value.length !== 0) {
+	                var v = angular.lowercase("" + value);
+	                value = !(v == 'f' || v == '0' || v == 'false' || v == 'no' || v == 'n' || v == '[]');
+	            } else {
+	                value = false;
+	            }
+	            return value;
+	        }
+	            
+	        var spinner,height = element.height(),
+                opts = {
+                    length: Math.round(height / 3),
+                    radius: Math.round(height / 5),
+                    width: Math.round(height / 10),
+                    color: element.css("color"),
+                    left: -5
+                };
+	            
+	            
+
+	        var first = true;
+	        scope.$watch('$coBusyTracker.' + options.tracker + '.active()', function (value) {
+	            //ignore first
+	            if (first) {
+	                first = false;
+	                return; 
+	            }
+	            if (toBoolean(value)) {
+	                spinner = new Spinner(opts).spin(element[0]);
+	                attrs.$set('disabled', true);
+	            } else {
+	                if (spinner) {
+	                    spinner.stop();
+	                }
+	                attrs.$set('disabled', false);
+	            }
+	        });
+	    }
+	};
+}]);
 
