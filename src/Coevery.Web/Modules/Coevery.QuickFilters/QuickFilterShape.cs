4760e99ff56f81de8413fc1bc8dfc52fbd1f0b3c
Zhang Junnan <zhangjn0430@gmail.com>
2013-12-27 10:31:05 +0800

    Modify quick filter

--- src/Coevery.Web/Modules/Coevery.QuickFilters/QuickFilterShape.cs
+++ src/Coevery.Web/Modules/Coevery.QuickFilters/QuickFilterShape.cs
@@ -1,31 +1,8 @@
-﻿using System.Globalization;
-using System.Linq;
-using System.Web.Mvc;
-using Coevery.ContentManagement;
-using Coevery.ContentManagement.MetaData;
-using Coevery.DisplayManagement.Descriptors;
-using Coevery.OptionSet.Services;
+﻿using Coevery.DisplayManagement.Descriptors;
 using Coevery.Projections.Descriptors.Filter;
-using Coevery.Projections.Services;
 
 namespace Coevery.QuickFilters {
     public class QuickFilterShape : IShapeTableProvider {
-        private readonly IProjectionManager _projectionManager;
-        private readonly IContentDefinitionManager _contentDefinitionManager;
-        private readonly IContentManager _contentManager;
-        private readonly IOptionSetService _optionSetService;
-
-        public QuickFilterShape(
-            IProjectionManager projectionManager,
-            IContentDefinitionManager contentDefinitionManager,
-            IContentManager contentManager,
-            IOptionSetService optionSetService) {
-            _projectionManager = projectionManager;
-            _contentDefinitionManager = contentDefinitionManager;
-            _contentManager = contentManager;
-            _optionSetService = optionSetService;
-        }
-
         public void Discover(ShapeTableBuilder builder) {
             builder.Describe("QuickFilter_ReferenceFilter")
                 .OnDisplaying(displaying => {
@@ -36,22 +13,8 @@ namespace Coevery.QuickFilters {
                     string partName = args[0];
                     string fieldName = args[1];
 
-                    var partDefinition = _contentDefinitionManager.GetPartDefinition(partName);
-                    if (partDefinition == null) {
-                        return;
-                    }
-                    var fieldDefinition = partDefinition.Fields.FirstOrDefault(x => x.Name == fieldName);
-                    if (fieldDefinition == null) {
-                        return;
-                    }
-                    var queryId = int.Parse(fieldDefinition.Settings["ReferenceFieldSettings.QueryId"]);
-
-                    shape.ListItems = _projectionManager
-                        .GetContentItems(queryId)
-                        .Select(x => new SelectListItem {
-                            Text = _contentManager.GetItemMetadata(x).DisplayText,
-                            Value = x.Id.ToString(CultureInfo.InvariantCulture)
-                        });
+                    shape.PartName = partName;
+                    shape.FieldName = fieldName;
                 });
 
             builder.Describe("QuickFilter_OptionSetFilter")
@@ -63,20 +26,8 @@ namespace Coevery.QuickFilters {
                     string partName = args[0];
                     string fieldName = args[1];
 
-                    var partDefinition = _contentDefinitionManager.GetPartDefinition(partName);
-                    if (partDefinition == null) {
-                        return;
-                    }
-                    var fieldDefinition = partDefinition.Fields.FirstOrDefault(x => x.Name == fieldName);
-                    if (fieldDefinition == null) {
-                        return;
-                    }
-                    var optionSetId = int.Parse(fieldDefinition.Settings["OptionSetFieldSettings.OptionSetId"]);
-                    shape.ListItems = _optionSetService.GetOptionItems(optionSetId)
-                        .Select(x => new SelectListItem {
-                            Text = x.Name,
-                            Value = x.Id.ToString(CultureInfo.InvariantCulture)
-                        });
+                    shape.PartName = partName;
+                    shape.FieldName = fieldName;
                 });
         }
     }
