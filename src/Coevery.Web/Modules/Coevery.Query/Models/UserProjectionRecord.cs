4121eec21cbbbf99b09f160e00b4cfe924a4949c
Zhang Junnan <zhangjn0430@gmail.com>
2014-01-02 18:26:10 +0800

    Commit Coevery.Query module

---
+++ src/Coevery.Web/Modules/Coevery.Query/Models/UserProjectionRecord.cs
@@ -0,0 +1,7 @@
+﻿namespace Coevery.Query.Models {
+    public class UserProjectionRecord {
+        public virtual int Id { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual int ProjectionId { get; set; }
+    }
+}
---
+++ src/Coevery.Web/Modules/Coevery.Query/Services/IQueryManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Coevery.ContentManagement;
+
+namespace Coevery.Query.Services {
+    public interface IQueryManager : IDependency {
+        IEnumerable<ContentItem> GetContentItems(int queryId, string contentTypeName, int skip = 0, int count = 0);
+        int GetCount(int queryId);
+    }
+}
