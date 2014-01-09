056e356637fcddbeabee16502ce0d2ee83185d56
Zhang Junnan <zhangjn0430@gmail.com>
2014-01-09 13:58:41 +0800

    Remove Query module

--- src/Coevery.Web/Modules/Coevery.Query/Services/IQueryManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Coevery.ContentManagement;
-
-namespace Coevery.Query.Services {
-    public interface IQueryManager : IDependency {
-        IEnumerable<ContentItem> GetContentItems(int queryId, string contentTypeName, int skip = 0, int count = 0);
-        int GetCount(int queryId);
-    }
-}
